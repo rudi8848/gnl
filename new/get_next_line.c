d0bd74390a764ed6cfe08d4cc86a6a2bd9f32cc5
Ganna VYNOGRADOVA <gvynogra@e1r3p4.unit.ua>
2017-12-25 13:23:35 +0200

    different return values with different buff-sizes

--- new/get_next_line.c
+++ new/get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvynogra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/25 12:50:56 by gvynogra          #+#    #+#             */
+/*   Updated: 2017/12/25 13:02:20 by gvynogra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 t_list		*fd_search(t_list *head, int fd)
@@ -11,9 +23,9 @@ t_list		*fd_search(t_list *head, int fd)
 	return (NULL);
 }
 
-void		push_file(t_list **head, int fd/*, char *content*/)
+void		push_file(t_list **head, int fd)
 {
-	t_list *tmp;
+	t_list			*tmp;
 
 	tmp = (t_list*)malloc(sizeof(t_list));
 	tmp->next = (*head);
@@ -22,71 +34,61 @@ void		push_file(t_list **head, int fd/*, char *content*/)
 	(*head) = tmp;
 }
 
-int	read_file(int const fd, void **data)
+int			read_file(int const fd, void **data)
 {
-	// printf("in read\n");
-	// printf("fd: %d, tmp->content: %s\n", fd, *data);
-	/*static*/ char	buff[BUFF_SIZE+1] = { '\n' };
-	int			read_bytes;
-	char        *nstr = NULL;
+	char			buff[BUFF_SIZE + 1];
+	int				read_bytes;
+	char			*nstr;
+
+	nstr = NULL;
 	read_bytes = read(fd, buff, BUFF_SIZE);
 	if (read_bytes > 0)
 	{
-	    buff[read_bytes] = '\0';
-	   // printf("buff: %s\n", buff);
-	    nstr = ft_strjoin(*data, buff);
-	    if (!nstr)
-	        return (-1);
-	     free(*data);
-	    *data = nstr;
+		buff[read_bytes] = '\0';
+		nstr = ft_strjoin(*data, buff);
+		if (!nstr)
+			return (-1);
+		free(*data);
+		*data = nstr;
 	}
-	// printf("nstr: %s\n", nstr);
-	// printf("read bytes: %d\n", read_bytes);
 	return (read_bytes);
 }
 
-int get_next_line(int const fd, char ** line)
+int			get_next_line(int const fd, char **line)
 {
-	//printf("in gnl\n");
-	///*static*/ char		*data = NULL;
 	static t_list	*file;
 	t_list			*tmp;
 	char			*remainder;
-	int         	bytes;
+	int				bytes;
 
 	if (!line || fd < 0 || (read(fd, 0, 0)) < 0)
 		return (-1);
 	if (!fd_search(file, fd))
 		push_file(&file, fd);
 	tmp = fd_search(file, fd);
-	//printf("before\n");
-	//tmp->content = data;
-	//printf("after\n");
-	if ((/*!data*/!(char*)tmp->content && !((tmp->content/*data*/ = (char *)ft_memalloc(sizeof(char))))) )
-	    return (-1);
-	
-	//printf("data: %s\n", data);
-	//printf("after if\n");
-	remainder = ft_strchr(/*data*/(char*)tmp->content, '\n');
-	//printf("remainder: %s\n", remainder);
+	if ((!(char*)tmp->content &&
+				!((tmp->content = (char *)ft_memalloc(sizeof(char))))))
+		return (-1);
+	remainder = ft_strchr((char*)tmp->content, '\n');
 	while (remainder == NULL)
 	{
-	//printf("in loop\n");
-	    bytes = read_file(fd, /*&data*/&tmp->content);
-	    if (bytes == 0)
-	    {
-	       if ((remainder = ft_strchr(/*data*/(char*)tmp->content, '\0')) == /*data*/(char*)tmp->content )
-	        return (0);
-	    } else if (bytes < 0)
-	        return (-1);
+		bytes = read_file(fd, &tmp->content);
+		if (bytes == 0)
+		{
+			if ((remainder = ft_strchr((char*)tmp->content, '\0'))
+					== (char*)tmp->content)
+				return (0);
+		}
+		else if (bytes < 0)
+			return (-1);
 		else
-			remainder = ft_strchr(/*data*/tmp->content, '\n');
+			remainder = ft_strchr(tmp->content, '\n');
 	}
-	*line = ft_strsub(/*data*/tmp->content, 0, remainder - /*data*/(char*)tmp->content);
-	   if (!*line)
-	    return (-1);
+	*line = ft_strsub(tmp->content, 0, remainder - (char*)tmp->content);
+	if (!*line)
+		return (-1);
 	remainder = ft_strdup(remainder + 1);
-	    free(/*data*/tmp->content);
-	/*data*/tmp->content = remainder;
+	free(tmp->content);
+	tmp->content = remainder;
 	return (1);
 }
--- new/get_next_line.h
+++ new/get_next_line.h
@@ -18,7 +18,7 @@
 # include <stdio.h>
 # include <unistd.h>
 
-# define BUFF_SIZE 10
+# define BUFF_SIZE 33
 
 int		get_next_line(const int fd, char **line);
 
