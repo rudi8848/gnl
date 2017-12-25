e4c81db16e76c1c774669c1bd0d9a7d877d19581
Ganna VYNOGRADOVA <gvynogra@e1r3p4.unit.ua>
2017-12-25 16:22:20 +0200

    35 lines in function

--- new/main.c
+++ new/main.c
@@ -11,43 +11,43 @@ int		main(void)
 
 	get_next_line(fd1, &line);
 		printf("1 - call file 1: %s\n", line);
-		printf("return: %d\n", get_next_line(fd1, &line));
+	//	printf("return: %d\n", get_next_line(fd1, &line));
 
 	get_next_line(fd2, &line);
 		printf("2 - call file 2:%s\n", line);
-		printf("return: %d\n", get_next_line(fd2, &line));
+	//	printf("return: %d\n", get_next_line(fd2, &line));
 
 	get_next_line(fd1, &line);
 		printf("3 - call file 1:%s\n", line);
-		printf("return: %d\n", get_next_line(fd1, &line));
+	//	printf("return: %d\n", get_next_line(fd1, &line));
 
 	get_next_line(fd3, &line);
 		printf("4 - call file 3: %s\n", line);
-		printf("return: %d\n", get_next_line(fd3, &line));
+	//	printf("return: %d\n", get_next_line(fd3, &line));
 
 	get_next_line(fd2, &line);
 		printf("5 - call file 2: %s\n", line);
-		printf("return: %d\n", get_next_line(fd2, &line));
+	//	printf("return: %d\n", get_next_line(fd2, &line));
 
 	get_next_line(fd1, &line);
 		printf("6 - call file 1: %s\n", line);
-		printf("return: %d\n", get_next_line(fd1, &line));
+	//	printf("return: %d\n", get_next_line(fd1, &line));
 
 	get_next_line(fd2, &line);
 		printf("7 - call file 2:%s\n", line);
-		printf("return: %d\n", get_next_line(fd2, &line));
+	//	printf("return: %d\n", get_next_line(fd2, &line));
 
 	get_next_line(fd1, &line);
 		printf("8 - call file 1:%s\n", line);
-		printf("return: %d\n", get_next_line(fd1, &line));
+	//	printf("return: %d\n", get_next_line(fd1, &line));
 
 	get_next_line(fd3, &line);
 		printf("9 - call file 3: %s\n", line);
-		printf("return: %d\n", get_next_line(fd3, &line));
+		//printf("return: %d\n", get_next_line(fd3, &line));
 
 	get_next_line(fd2, &line);
 		printf("10 - call file 2: %s\n", line);
-		printf("return: %d\n", get_next_line(fd2, &line));
+	//	printf("return: %d\n", get_next_line(fd2, &line));
 
 	close(fd1);
 	close(fd2);
